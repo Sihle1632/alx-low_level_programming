830ca2fd3f60da3cb3e2ea5260463153f95f18e1
Sihle1632 <sihlebudaza@gmail.com>
2023-09-19 16:35:09 -0700

    0-main.c

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
     int n;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+        n = 402;
+    	printf("n=%d\n", n);
+        reset_to_98(&n);
+        printf("n=%d\n", n);
+        return (0);
 }
